356a21420650be8a3bf70638862d148cd88a76a8
ewaldmikolaj <mikolajewald@gmail.com>
2020-12-23 17:21:58 +0100

    read from file done better way

--- projekt_bibilioteka/main.cpp
+++ projekt_bibilioteka/main.cpp
@@ -6,31 +6,32 @@
 
 //structs
 
-typedef struct user {
+struct User {
   std::string name;
   std::string surname;
-} user;
+};
 
-typedef struct date {
+struct Date {
   int day;
   int month;
   int year;
-} date;
+};
 
-typedef struct book {
+struct Book {
   int id;
   std::string title;
-  user author;
+  struct User author;
   std::string cathegory;
-  date hire_date;
-  user reader;
-} book;
+  struct Date hire_date;
+  struct User reader;
+  std::string others;
+};
 
-typedef struct list_of_books {
-  book book;
+struct list_of_books {
+  struct Book book;
   list_of_books* next;
   list_of_books* prev;
-} list_of_books;
+};
 
 // global variables
 
@@ -38,37 +39,29 @@ list_of_books* head = NULL;
 
 // functions
 
-void push(book data);
+void push(Book data);
 void print_all();
 
-void print_book(book data);
-void save_to_file(book data);
+void print_book(Book data);
+void save_to_file();
 void read_from_file();
 
 //main
 
 int main() {
   read_from_file();
-  // book jeden;
-  // jeden.id = 1;
-  // jeden.title = "w pustyni";
-  // jeden.author.name = "wiktor";
-  // jeden.author.surname = "szymanski";
-  // jeden.cathegory = "dramat";
-  // jeden.hire_date.day = 1;
-  // jeden.hire_date.month = 1;
-  // jeden.hire_date.year = 2020;
-  // jeden.reader.name = "mikołaj";
-  // jeden.reader.surname = "ewald";
-  // push(jeden);
+  // Book a = {1, "20 pytan", "no", "hai", "popularnonaukowa", 2, 2, 2020, "mikołaj", "ewald", "bardzo fajna"};
+  // Book b = {2, "21 pytan", "noah", "harari", "naukowa", 2, 2, 2020, "mikołaj", "ewald", "bardzo fajna"};
+  //push(a);
+  //push(b);
   print_all();
-  //save_to_file(jeden);
+  save_to_file();
   return 0;
 }
 
 //functions list
 
-void push(book data) {
+void push(Book data) {
   if (head == NULL) {
     list_of_books* element = new list_of_books;
     element->book = data;
@@ -98,48 +91,36 @@ void print_all() {
   }
 }
 
+void save_to_file() {
+  std::fstream database;
+  database.open("database.txt", std::fstream::out);
+  list_of_books* element = head;
+  while(element != NULL) {
+    database << element->book.id << "\n" << element->book.title << "\n" << element->book.author.name << "\n" << element->book.author.surname << "\n" << element->book.cathegory << "\n" << element->book.hire_date.day << "\n" << element->book.hire_date.month << "\n" << element->book.hire_date.year << "\n" << element->book.reader.name << "\n" << element->book.reader.surname << "\n" << element->book.others << "\n";
+    element = element->next;
+  }
+  database.close();
+}
+
 //functions others
 
-void print_book(book data) {
+void print_book(Book data) {
   std::cout << data.author.name << " " << data.author.surname << "\n";
 }
 
 void read_from_file() {
   std::ifstream database("database.txt");
   if (database.good()) {
-    book from_database;
-    std::string variable;
+    std::string tab[11];
+    int i = 0;
     while(!database.eof()) {
-      getline(database, variable);
-      from_database.id = std::stoi(variable);
-      getline(database, variable);
-      from_database.title = variable;
-      getline(database, variable);
-      from_database.author.name = variable;
-      getline(database, variable);
-      from_database.author.surname = variable;
-      getline(database, variable);
-      from_database.cathegory = variable;
-      getline(database, variable);
-      from_database.hire_date.day = std::stoi(variable);
-      getline(database, variable);
-      from_database.hire_date.month = std::stoi(variable);
-      getline(database, variable);
-      from_database.hire_date.year = std::stoi(variable);
-      getline(database, variable);
-      from_database.reader.name = variable;
-      getline(database, variable);
-      from_database.reader.surname = variable;
-      push(from_database);
+      for (auto &value : tab) {
+        getline(database, value);
+      }
+      Book temporary = { std::stoi(tab[0]), tab[1], tab[2], tab[3], tab[4], std::stoi(tab[5]), std::stoi(tab[6]), std::stoi(tab[7]), tab[8], tab[9], tab[10]};
+      push(temporary);
     }
   } else {
     printf("nie ma bazy\n");
   }
-}
-
-void save_to_file(book data) {
-  std::fstream database;
-  database.open("database.txt", std::fstream::out);
-  database << data.id << "\n" << data.title << "\n" << data.author.name << "\n" << data.author.surname << "\n" << data.cathegory << "\n" << data.hire_date.day << "\n" << data.hire_date.month << "\n" << data.hire_date.year << "\n" << data.reader.name << "\n" << data.reader.surname;
-  database.close();
 }
